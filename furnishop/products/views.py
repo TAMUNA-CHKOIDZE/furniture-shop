0ae58c9af4651eff277fc13f24e1408b633f6d23
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-11-02 16:27:37 +0400

    ProductListView and ProductDetailView views

--- furnishop/products/views.py
+++ furnishop/products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from products.filters import ProductFilter
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+# ყველა პროდუქტის ვიუ (ლისტინგი)
+class ProductListView(generics.ListAPIView):
+    # API-ის მიერ დაბრუნებული მონაცემები იქნებიან ყველა Product ობიექტი ბაზაში, თუმცა მხოლოდ ისინი გამოჩნდება რომლებიც ხელმისაწვდომია
+    queryset = Product.objects.filter(is_available=True)
+    # მონაცემები ProductSerializer-ის მეშვეობით გადაიყვანება JSON ფორმატში, Serializer იღებს Product ობიექტებს და JSON-ის სახით უგზავნის მომხმარებელს.
+    serializer_class = ProductSerializer
+    # ფილტრაცია შესაძლებელია კატეგორიით, ფერით, მასალით
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ProductFilter  # filterset-ის გადატვირთვა ფილტრის ლოგიკისთვის
+    ordering_fields = ['price', 'created_at']  # დალაგება ფასის და შექმნის თარიღის მიხედვით ხდება
+
+# კონკრეტული პროდუქტი
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
