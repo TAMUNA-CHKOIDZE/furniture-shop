2533023d49fb185896efac60daf2edc4bb8b188d
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-11-02 16:23:27 +0400

    CategoryListView and CategoryDetailView views

--- furnishop/categories/views.py
+++ furnishop/categories/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from categories.models import Category
+from categories.serializers import CategorySerializer
+
+
+# ყველა კატეგორია
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# კონკრეტული კატეგორია
+class CategoryDetailView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
